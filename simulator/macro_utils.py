51d5ad8ffcd766989a0bfd054c6ec6a3b2294a7c
wangyiou <wangyiou@xiaohongshu.com>
2024-07-22 21:01:19 +0800

    feat: simulator: pim_compute group

--- simulator/macro_utils.py
+++ simulator/macro_utils.py
@@ -11,14 +11,17 @@ class MacroConfig:
         assert self.n_bcol % bitwidth == 0
         return self.n_bcol // bitwidth
 
+    def total_size(self):
+        return self.n_macro * self.n_row * self.n_comp * self.n_bcol // 8
+
 class MacroUtil:
     def __init__(self, macro_memory, macro_config):
         self.macro_memory = macro_memory
         self.macro_config = macro_config
         
-    def extract_macro_structure_from_memory(self, data_type):
+    def extract_macro_structure_from_memory(self, data_type, group_num):
         """
-        <N_ROW, N_COMP, N_MACRO, N_VCOL>
+        <N_ROW, N_COMP, N_GROUP, N_MACRO_PER_GROUP, N_VCOL>
         """
         if type(data_type)==int:
             bitwidth = data_type
@@ -30,21 +33,29 @@ class MacroUtil:
             bitwidth = get_bitwidth_from_dtype(data_type)
         else:
             assert False, f"Unsupport {data_type=}"
+        assert self.macro_config.n_macro % group_num == 0, f"{self.macro_config.n_macro=}, {group_num=}"
         data_bytes = self.macro_memory.read_all()
         macro = np.frombuffer(data_bytes, dtype=data_type)
         macro = macro.reshape(self.macro_config.n_row, 
                             self.macro_config.n_comp, 
-                            self.macro_config.n_macro, 
+                            group_num, 
+                            self.macro_config.n_macro // group_num, 
                             self.macro_config.n_vcol(bitwidth))
         return macro
 
     def get_macro_data(self, 
         activate_row,
         data_type,
+        group_num,
         activate_element_row_num,
-        activate_element_col_num):
-        
-        macro = self.extract_macro_structure_from_memory(data_type)
-        macro = macro.reshape(*macro.shape[:2], -1)
-        data = macro[activate_row, :activate_element_row_num, :activate_element_col_num]
+        activate_element_col_num,
+        activate_group_num):
+
+        assert self.macro_config.n_macro % group_num == 0, f"{self.macro_config.n_macro=}, {group_num=}"
+        assert 0 <= activate_group_num and activate_group_num <= group_num, f"{activate_group_num=}, {group_num=}"
+
+        macro = self.extract_macro_structure_from_memory(data_type, group_num)
+        print(f"{macro.shape=}")
+        macro = macro.reshape(*macro.shape[:3], -1)
+        data = macro[activate_row, :activate_element_row_num, :activate_group_num, :activate_element_col_num]
         return data
--- test/simulator/test_pimcompute.py
+++ test/simulator/test_pimcompute.py
@@ -3,13 +3,18 @@ from test.simulator.utils import InstUtil
 from simulator.simulator import MemorySpace, Memory, Simulator, SpecialReg
 from simulator.macro_utils import MacroConfig
 import numpy as np
-def init_memory_space():
+
+def init_macro_config():
+    macro_config = MacroConfig(n_macro=2, n_row=4, n_comp=4, n_bcol=16)
+    return macro_config
+    
+def init_memory_space(macro_config):
     memory_space = MemorySpace()
     global_memory = Memory("global_memory", "dram", 0, 128)
     local_memory = Memory("local_memory", "sram", 128, 128)
     input_buffer = Memory("input_buffer", "rf", 256, 64)
     output_buffer = Memory("output_buffer", "rf", 320, 64)
-    macro = Memory("macro", "macro", 384, 2*4*4*16//8)
+    macro = Memory("macro", "macro", 384, macro_config.total_size())
     memory_space.add_memory(global_memory)
     memory_space.add_memory(local_memory)
     memory_space.add_memory(input_buffer)
@@ -17,17 +22,13 @@ def init_memory_space():
     memory_space.add_memory(macro)
     return memory_space
 
-def init_macro_config():
-    macro_config = MacroConfig(n_macro=2, n_row=4, n_comp=4, n_bcol=16)
-    return macro_config
-
 class TestSimulatorPIMCompute:
 
     @classmethod
     def setup_class(cls):
         cls.inst_util = InstUtil()
-        cls.memory_space = init_memory_space()
         cls.macro_config = init_macro_config()
+        cls.memory_space = init_memory_space(cls.macro_config)
         cls.simulator = Simulator(cls.memory_space , cls.macro_config)
 
     def setup_method(self):
@@ -61,6 +62,10 @@ class TestSimulatorPIMCompute:
             self.inst_util.special_li(SpecialReg.WEIGHT_BIT_WIDTH, 8),
             self.inst_util.special_li(SpecialReg.OUTPUT_BIT_WIDTH, 32),
             self.inst_util.special_li(SpecialReg.ACTIVATION_ELEMENT_COL_NUM, 4),
+            
+            self.inst_util.special_li(SpecialReg.GROUP_SIZE, self.macro_config.n_macro),
+            self.inst_util.special_li(SpecialReg.ACTIVATION_GROUP_NUM, 1),
+            self.inst_util.special_li(SpecialReg.GROUP_INPUT_STEP, 0),
 
             self.inst_util.pimcompute_dense_single_group(
                 0, # accumulate
@@ -81,7 +86,7 @@ class TestSimulatorPIMCompute:
         assert status==self.simulator.FINISH
         
         output = self.simulator.memory_space.read_as(output_addr, 16, np.int32)
-        assert (output==output_golden).all()
+        assert (output==output_golden).all(), f"{output=}, {output_golden=}"
 
     def test_pimcompute_dense_single_group_accumulate(self):
         """
@@ -112,6 +117,10 @@ class TestSimulatorPIMCompute:
             self.inst_util.special_li(SpecialReg.WEIGHT_BIT_WIDTH, 8),
             self.inst_util.special_li(SpecialReg.OUTPUT_BIT_WIDTH, 32),
             self.inst_util.special_li(SpecialReg.ACTIVATION_ELEMENT_COL_NUM, 4),
+
+            self.inst_util.special_li(SpecialReg.GROUP_SIZE, self.macro_config.n_macro),
+            self.inst_util.special_li(SpecialReg.ACTIVATION_GROUP_NUM, 1),
+            self.inst_util.special_li(SpecialReg.GROUP_INPUT_STEP, 0),
 
             self.inst_util.pimcompute_dense_single_group(
                 0, # accumulate
@@ -177,6 +186,10 @@ class TestSimulatorPIMCompute:
             self.inst_util.special_li(SpecialReg.OUTPUT_BIT_WIDTH, 32),
             self.inst_util.special_li(SpecialReg.ACTIVATION_ELEMENT_COL_NUM, 3),
 
+            self.inst_util.special_li(SpecialReg.GROUP_SIZE, self.macro_config.n_macro),
+            self.inst_util.special_li(SpecialReg.ACTIVATION_GROUP_NUM, 1),
+            self.inst_util.special_li(SpecialReg.GROUP_INPUT_STEP, 0),
+
             self.inst_util.pimcompute_dense_single_group(
                 0, # accumulate
                 0, # rs1 input addr
@@ -201,10 +214,153 @@ class TestSimulatorPIMCompute:
         assert (output[:3]==output_golden).all()
         assert output[3]==0
 
+    def test_pimcompute_dense_multi_group_fix_step(self):
+        """
+        batched mvm
+        \forall b: y[b,:] = x[b,:] \cdot A[b,:,:]
+        x: int8, shape=[2, 4], memory=local, addr=LOCAL_BASE, size=8
+        A: int8, shape=[4, 2, 2], memory=macro, addr=MACRO_BASE, size=16
+        y: int32, shape=[2, 2], memory=local, addr=LOCAL_BASE+8, size=16
+        """
+
+        input_buffer_base = self.simulator.memory_space.get_base_of("input_buffer")
+        output_buffer_base = self.simulator.memory_space.get_base_of("output_buffer")
+        macro_base = self.simulator.memory_space.get_base_of("macro")
+
+        input_addr = input_buffer_base
+        input_size = 4
+        weight_addr = macro_base
+        weight_size = 16
+        output_addr = output_buffer_base
+
+        group_num = 2
+
+        inst_list = [
+            # set general register
+            self.inst_util.general_li(0, input_addr), # input addr
+            self.inst_util.general_li(1, input_size), # input size
+            self.inst_util.general_li(2, 0), # activate row
+            self.inst_util.general_li(3, output_addr), # output addr
+
+            # set special register
+            self.inst_util.special_li(SpecialReg.INPUT_BIT_WIDTH, 8),
+            self.inst_util.special_li(SpecialReg.WEIGHT_BIT_WIDTH, 8),
+            self.inst_util.special_li(SpecialReg.OUTPUT_BIT_WIDTH, 32),
+            self.inst_util.special_li(SpecialReg.ACTIVATION_ELEMENT_COL_NUM, 2),
+
+            self.inst_util.special_li(SpecialReg.GROUP_SIZE, self.macro_config.n_macro // group_num),
+            self.inst_util.special_li(SpecialReg.ACTIVATION_GROUP_NUM, group_num),
+            self.inst_util.special_li(SpecialReg.GROUP_INPUT_STEP, input_size),
+
+            self.inst_util.pimcompute_dense_single_group(
+                0, # accumulate
+                0, # rs1 input addr
+                1, # rs2 input size
+                2, # rs3 activate row
+                3, # rd output addr
+            )
+        ]
+        group_input = np.arange(8, dtype=np.int8).reshape(2,4)
+        group_weight = np.arange(16, dtype=np.int8).reshape(4,2,2) # [comp, group, macro_per_group]
+        group_output_golden = []
+        for group_id in range(group_num):
+            input = group_input[group_id]
+            weight = group_weight[:,group_id,:]
+            output_golden = np.dot(input.astype(np.int32), weight.astype(np.int32))
+            group_output_golden.append(output_golden)
+
+        self.simulator.memory_space.write(group_input, input_addr, input_size * group_num)
+        self.simulator.memory_space.write(group_weight, weight_addr, weight_size)
+        status = self.simulator.run_code(inst_list)
+
+        assert status==self.simulator.FINISH
+        
+        output = self.simulator.memory_space.read_as(output_addr, 16, np.int32)
+        group_output_golden = np.concatenate(group_output_golden, axis=0).reshape(-1)
+        assert (output==group_output_golden).all(), f"{output=}, {group_output_golden=}"
+
+    def test_pimcompute_dense_multi_group_accumulate_fix_step(self):
+        """
+        batched mvm
+        \forall b: y[b,:] = x[b,:] \cdot A[b,:,:] + x[b,:] \cdot B[b,:,:]
+        x: int8, shape=[2, 4], memory=local, addr=LOCAL_BASE, size=8
+        A: int8, shape=[4, 2, 2], memory=macro, addr=MACRO_BASE, size=16
+        B: int8, shape=[4, 2, 2], memory=macro, addr=MACRO_BASE+16, size=16
+        y: int32, shape=[2, 2], memory=local, addr=LOCAL_BASE+8, size=16
+        """
+
+        input_buffer_base = self.simulator.memory_space.get_base_of("input_buffer")
+        output_buffer_base = self.simulator.memory_space.get_base_of("output_buffer")
+        macro_base = self.simulator.memory_space.get_base_of("macro")
+
+        input_addr = input_buffer_base
+        input_size = 4
+        weight_addr = macro_base
+        weight_size = 32
+        output_addr = output_buffer_base
+
+        group_num = 2
+
+        inst_list = [
+            # set general register
+            self.inst_util.general_li(0, input_addr), # input addr
+            self.inst_util.general_li(1, input_size), # input size
+            self.inst_util.general_li(2, 0), # activate row
+            self.inst_util.general_li(3, output_addr), # output addr
+
+            # set special register
+            self.inst_util.special_li(SpecialReg.INPUT_BIT_WIDTH, 8),
+            self.inst_util.special_li(SpecialReg.WEIGHT_BIT_WIDTH, 8),
+            self.inst_util.special_li(SpecialReg.OUTPUT_BIT_WIDTH, 32),
+            self.inst_util.special_li(SpecialReg.ACTIVATION_ELEMENT_COL_NUM, 2),
+
+            self.inst_util.special_li(SpecialReg.GROUP_SIZE, self.macro_config.n_macro // group_num),
+            self.inst_util.special_li(SpecialReg.ACTIVATION_GROUP_NUM, group_num),
+            self.inst_util.special_li(SpecialReg.GROUP_INPUT_STEP, input_size),
+
+            self.inst_util.pimcompute_dense_single_group(
+                0, # accumulate
+                0, # rs1 input addr
+                1, # rs2 input size
+                2, # rs3 activate row
+                3, # rd output addr
+            ),
+
+            self.inst_util.general_li(2, 1), # activate row
+            self.inst_util.pimcompute_dense_single_group(
+                1, # accumulate
+                0, # rs1 input addr
+                1, # rs2 input size
+                2, # rs3 activate row
+                3, # rd output addr
+            )
+        ]
+        group_input = np.arange(8, dtype=np.int8).reshape(2,4)
+        group_weight = np.arange(32, dtype=np.int8).reshape(2,4,2,2) # [row, comp, group, macro_per_group]
+        group_output_golden = []
+        for group_id in range(group_num):
+            input = group_input[group_id]
+            weight0 = group_weight[0,:,group_id,:]
+            weight1 = group_weight[1,:,group_id,:]
+            output0 = np.dot(input.astype(np.int32), weight0.astype(np.int32))
+            output1 = np.dot(input.astype(np.int32), weight1.astype(np.int32))
+            output_golden = output0 + output1
+            group_output_golden.append(output_golden)
+
+        self.simulator.memory_space.write(group_input, input_addr, input_size * group_num)
+        self.simulator.memory_space.write(group_weight, weight_addr, weight_size)
+        status = self.simulator.run_code(inst_list)
+
+        assert status==self.simulator.FINISH
+        
+        output = self.simulator.memory_space.read_as(output_addr, 16, np.int32)
+        group_output_golden = np.concatenate(group_output_golden, axis=0).reshape(-1)
+        assert (output==group_output_golden).all(), f"{output=}, {group_output_golden=}"
 
 if __name__=="__main__":
     TestSimulatorPIMCompute.setup_class()
     test_simulator = TestSimulatorPIMCompute()
-    test_simulator.test_pimcompute_dense_single_group()
-    test_simulator.test_pimcompute_dense_single_group_accumulate()
-    test_simulator.test_pimcompute_dense_single_group_part()
+    test_simulator.setup_method()
+    test_simulator.test_pimcompute_dense_multi_group_accumulate_fix_step()
+    # test_simulator.test_pimcompute_dense_single_group_accumulate()
+    # test_simulator.test_pimcompute_dense_single_group_part()
