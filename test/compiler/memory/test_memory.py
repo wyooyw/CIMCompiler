8ed179efc9e67ac2a9e92a54103f1b3b7a53b0a3
wangyiou <wangyiou@xiaohongshu.com>
2024-07-24 14:05:33 +0800

    test: memory

--- test/compiler/memory/test_memory.py
+++ test/compiler/memory/test_memory.py
@@ -43,6 +43,8 @@ class TestMemory:
         self.simulator.clear()
 
     @pytest.mark.parametrize('casename',[
+        'load_save', 'load_save_var_index',
+        'slice', 'slice_var_index', 'slice_chain',
         'trans'
         ])
     def test_memory_with_print(self, casename):
