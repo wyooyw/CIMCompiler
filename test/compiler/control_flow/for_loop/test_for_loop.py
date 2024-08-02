63f17879530b0dd1494b48472cd0a5e45d1ec735
wangyiou <wangyiou12@163.com>
2024-08-02 01:41:43 -0700

    test: twin loop

--- test/compiler/control_flow/for_loop/test_for_loop.py
+++ test/compiler/control_flow/for_loop/test_for_loop.py
@@ -33,7 +33,7 @@ class TestForLoop:
     @pytest.mark.parametrize('casename',[
         'print_in_loop','count_in_loop','accumulate_in_loop', 
         'print_in_double_loop', 'count_in_double_loop', 'accumulate_in_double_loop',
-        'fibonacci'
+        'fibonacci', 'twin_loop'
         ])
     def test_control_flow(self, casename):
         case_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)), casename)
