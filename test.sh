f3048e7caeefd7e25ad41b034f8f27fa5ec705e9
wangyiou <wangyiou12@163.com>
2024-09-13 06:04:21 -0700

    fix

--- test.sh
+++ test.sh
@@ -1,2 +1,8 @@
 export PYTHONPATH=${PWD}
-pytest test
+export FAST_MODE=0
+export IM2COL_SMALL_INPUT_MEMORY=1
+# pytest test
+# pytest test/compiler/pimcompute/test_pimcompute_dwconv.py
+# pytest test/compiler/pimcompute/test_pimcompute_linear.py
+pytest test/compiler/pimcompute/test_pimcompute_value_sparse.py
+# python test/compiler/control_flow/for_loop/test_for_loop.py
