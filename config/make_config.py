95bb1c82b354f89ffa5be3556ca2bbddba98eb5f
wangyiou <wangyiou12@163.com>
2024-12-05 07:56:56 -0800

    feat：select config template

--- config/make_config.py
+++ config/make_config.py
@@ -1,8 +1,8 @@
 import json
 import os
+import argparse
 
-
-def make_config():
+def make_config(args):
     """
     1.read config_template.json
     2.calculate each memory's offset, fill into 'offset_byte'
@@ -10,9 +10,11 @@ def make_config():
     """
     import json
 
-    config_template_path = os.path.join(
-        os.path.dirname(__file__), "config_template.json"
-    )
+    config_template_path = args.config_template_path
+    # if it is relative path, convert it to absolute path
+    if not os.path.isabs(config_template_path):
+        config_template_path = os.path.join(os.path.dirname(__file__), config_template_path)
+
     config_path = os.path.join(os.path.dirname(__file__), "config.json")
     with open(config_template_path, "r") as f:
         config = json.load(f)
@@ -21,6 +23,8 @@ def make_config():
         addressing = memory["addressing"]
         addressing["offset_byte"] = last_end
         last_end += addressing["size_byte"]
+    
+    print(f"config template path: {config_template_path}")
     if os.path.exists(config_path):
         make_sure = input(
             f"config.json already exists, do you want to overwrite it? (y/n)"
@@ -30,6 +34,11 @@ def make_config():
             json.dump(config, f, indent=4)
         print("make config.json done")
 
+def parse_args():
+    parser = argparse.ArgumentParser(description="make config.json")
+    parser.add_argument("--config-template-path", "-p", type=str, help="config template path")
+    return parser.parse_args()
 
 if __name__ == "__main__":
-    make_config()
+    args = parse_args()
+    make_config(args)
