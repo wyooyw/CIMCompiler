48d18fa93b582e94462faebd551108cb7d867e9d
wangyiou <wangyiou12@163.com>
2025-02-05 10:34:40 +0800

    refactor: reorganize precompile

---
+++ precompile/__init__.py
@@ -0,0 +1,9 @@
+from precompile.remove_comment import remove_comments, remove_comments_from_file
+from precompile.macro_replace import detect_and_replace_macros, detect_and_replace_macros_from_file
+
+__all__ = [
+    "remove_comments",
+    "remove_comments_from_file",
+    "detect_and_replace_macros",
+    "detect_and_replace_macros_from_file",
+]
--- precompile/remove_comment.py
+++ precompile/remove_comment.py
@@ -3,20 +3,45 @@ from utils.logger import get_logger
 
 logger = get_logger(__name__)
 
-if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description="Remove comments from a file")
-    parser.add_argument("--in-file", type=str, help="File to remove comments from")
-    parser.add_argument("--out-file", type=str, help="File to remove comments from")
-    args = parser.parse_args()
-    new_line_list = []
+def remove_comments(code):
+    lines = code.split("\n")
+    new_lines = []
+    for line in lines:
+        line = line.strip()
+        if "//" in line:
+            line = line[: line.index("//")]
+        line = line + "\n"
+        new_lines.append(line)
+    return "".join(new_lines)
+
+def remove_comments_from_file(args):
     with open(args.in_file, "r") as f:
-        for line in f:
-            if "//" in line:
-                line = line[: line.index("//")]
-                line = line + "\n"
-            new_line_list.append(line)
+        code = f.read()
+    new_code = remove_comments(code)
     with open(args.out_file, "w") as f:
-        f.write("".join(new_line_list))
+        f.write(new_code)
     logger.debug(
         f"Comments removed from file \n{args.in_file}\n and saved to \n{args.out_file}"
     )
+
+def parse_args():
+    parser = argparse.ArgumentParser(description="Remove comments from a file")
+    parser.add_argument(
+        "--in-file", 
+        "-i", 
+        type=str, 
+        required=True,
+        help="Input file to remove comments from"
+    )
+    parser.add_argument(
+        "--out-file", 
+        "-o", 
+        type=str, 
+        required=True,
+        help="Output file to save the processed content"
+    )
+    return parser.parse_args()
+
+if __name__ == "__main__":
+    args = parse_args()
+    remove_comments_from_file(args)
