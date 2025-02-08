5ba55f54ac107e8fe88869cc0a7e9fb8b5e68092
wangyiou <wangyiou12@163.com>
2025-02-08 12:55:57 +0800

    feat: setup

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="cim_compiler",
+    version="0.1.0",
+    packages=find_packages(),
+    entry_points={
+        'console_scripts': [
+            'cim-compiler=cim_compiler.cli.main:main',
+        ],
+    },
+    install_requires=[
+        # 在这里列出你的依赖包
+    ],
+)
