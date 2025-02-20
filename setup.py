0165eb9cbee125db2cbd4698604f133ef8da8620
wangyiou <wangyiou12@163.com>
2025-02-20 13:39:12 +0800

    feat: replace cim-compiler to  cmcp for a shorter CLI

--- setup.py
+++ setup.py
@@ -7,6 +7,7 @@ setup(
     entry_points={
         'console_scripts': [
             'cim-compiler=cim_compiler.cli.main:main',
+            'cmcp=cim_compiler.cli.main:main',
         ],
     },
     install_requires=[
