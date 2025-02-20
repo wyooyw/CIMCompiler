1f4782f547bc64d791ff5172c6ff3a5e3e360b2d
wangyiou <wangyiou12@163.com>
2025-02-20 14:01:42 +0800

    fix: open tests

--- test/op/test_conv.py
+++ test/op/test_conv.py
@@ -22,49 +22,49 @@ class TestConv(TestBase):
         "op_config",
         [
             {"out_channel": 4, "in_channel": 3, "ker_size": 3, "in_hw": 8, "out_hw": 6},
-            # *[{
-            #     "out_channel": ch_out,
-            #     "in_channel": ch_in,
-            #     "ker_size": 3,
-            #     "in_hw": 8,
-            #     "out_hw": 6,
-            # } for (ch_out, ch_in) in [
-            #     (16, 3), (32, 16), (64, 16), (384, 16), (256, 96)
-            # ]],
-            # *[{
-            #     "out_channel": ch_out,
-            #     "in_channel": ch_in,
-            #     "ker_size": 3,
-            #     "in_hw": 4,
-            #     "out_hw": 2,
-            # } for (ch_out, ch_in) in [
-            #     (16, 384), (256, 384)
-            # ]],
+            *[{
+                "out_channel": ch_out,
+                "in_channel": ch_in,
+                "ker_size": 3,
+                "in_hw": 8,
+                "out_hw": 6,
+            } for (ch_out, ch_in) in [
+                (16, 3), (32, 16), (64, 16), (384, 16), (256, 96)
+            ]],
+            *[{
+                "out_channel": ch_out,
+                "in_channel": ch_in,
+                "ker_size": 3,
+                "in_hw": 4,
+                "out_hw": 2,
+            } for (ch_out, ch_in) in [
+                (16, 384), (256, 384)
+            ]],
 
-            # # stride=2 配置
-            # *[{
-            #     "out_channel": ch_out,
-            #     "in_channel": ch_in,
-            #     "ker_size": 3,
-            #     "in_hw": in_hw,
-            #     "out_hw": out_hw,
-            #     "padding": 1,
-            #     "stride": 2,
-            # } for (ch_out, ch_in, in_hw, out_hw) in [
-            #     (32, 16, 8, 4), (64, 16, 8, 4),
-            #     (16, 384, 4, 2), (384, 16, 8, 4)
-            # ]],
+            # stride=2 配置
+            *[{
+                "out_channel": ch_out,
+                "in_channel": ch_in,
+                "ker_size": 3,
+                "in_hw": in_hw,
+                "out_hw": out_hw,
+                "padding": 1,
+                "stride": 2,
+            } for (ch_out, ch_in, in_hw, out_hw) in [
+                (32, 16, 8, 4), (64, 16, 8, 4),
+                (16, 384, 4, 2), (384, 16, 8, 4)
+            ]],
             
-            # # 1x1 卷积配置
-            # *[{
-            #     "out_channel": ch_out,
-            #     "in_channel": ch_in,
-            #     "ker_size": 1,
-            #     "in_hw": 1,
-            #     "out_hw": 1,
-            # } for (ch_out, ch_in) in [
-            #     (32, 16), (64, 16), (16, 384), (384, 16)
-            # ]],
+            # 1x1 卷积配置
+            *[{
+                "out_channel": ch_out,
+                "in_channel": ch_in,
+                "ker_size": 1,
+                "in_hw": 1,
+                "out_hw": 1,
+            } for (ch_out, ch_in) in [
+                (32, 16), (64, 16), (16, 384), (384, 16)
+            ]],
         ],
     )
     def test_pim_compute(self, casename, op_config):
@@ -74,8 +74,8 @@ class TestConv(TestBase):
         self.run_op_test(casename, op_config, pimcompute_count)
 
 if __name__ == "__main__":
-    TestPIMComputeValueSparse.setup_class()
-    tester = TestPIMComputeValueSparse()
+    TestConv.setup_class()
+    tester = TestConv()
     tester.setup_method()
     tester.test_pim_compute(
         "conv2d/dense/quantize",
