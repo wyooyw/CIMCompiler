316eaf6380bb27fae3093174a69869cb228f057d
wangyiou <wangyiou12@163.com>
2025-03-08 23:02:02 +0800

    enhance: move antlr compile into build.sh

--- build.sh
+++ build.sh
@@ -6,6 +6,7 @@ start=$(date +%s)
 
 mkdir build
 cd build
+# build cim-compiler
 cmake -G Ninja .. \
     -DMLIR_DIR=$PREFIX/lib/cmake/mlir \
     -DLLVM_EXTERNAL_LIT=$BUILD_DIR/bin/llvm-lit \
@@ -19,6 +20,13 @@ cmake -G Ninja .. \
 cmake --build . --target main
 
     # -DCMAKE_BUILD_TYPE=Debug
+
+# build antlr
+cd ..
+mkdir -p build/antlr
+java -cp cim_compiler/java/antlr-4.7.1-complete.jar org.antlr.v4.Tool CIM.g -o build/antlr
+cp cim_compiler/java/AntlrToJson.java build/antlr
+javac -cp "cim_compiler/java/*" build/antlr/CIM*.java build/antlr/AntlrToJson.java
 
 end=$(date +%s)
 echo "Running time: $((end-start)) seconds"
