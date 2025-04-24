23c5a7669a6daaf849c18948b675e455b2496820
wangyiou <wangyiou12@163.com>
2025-04-25 00:10:39 +0800

    fix: scripts

--- scripts/run_model_efficientnet.sh
+++ scripts/run_model_efficientnet.sh
@@ -12,12 +12,14 @@ model_path_value_bit_sparse_0_4="${CIM_COMPILER_BASE}/models/efficientnet/Effici
 model_path_value_bit_sparse_0_2="${CIM_COMPILER_BASE}/models/efficientnet/EfficientNet_0.2_csd_th2_data_0704"
 quantify=true
 
-python engine/model_runner.py \
+python cim_compiler/engine/model_runner.py \
 --model_name $model_name \
---model_path_dense $model_path_dense \
---model_path_bit_sparse $model_path_bit_sparse \
+--quantify $quantify \
 --model_path_value_sparse $model_path_value_sparse \
 --model_path_value_bit_sparse_0_6 $model_path_value_bit_sparse_0_6 \
---model_path_value_bit_sparse_0_4 $model_path_value_bit_sparse_0_4 \
---model_path_value_bit_sparse_0_2 $model_path_value_bit_sparse_0_2 \
---quantify $quantify
+# --model_path_bit_sparse $model_path_bit_sparse \
+# --model_path_bit_sparse $model_path_bit_sparse \
+# --model_path_value_sparse $model_path_value_sparse \
+# --model_path_value_bit_sparse_0_6 $model_path_value_bit_sparse_0_6 \
+# --model_path_value_bit_sparse_0_4 $model_path_value_bit_sparse_0_4 \
+# --model_path_value_bit_sparse_0_2 $model_path_value_bit_sparse_0_2 \
