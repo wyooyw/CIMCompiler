4ad0b49b317eaa17778883a06dbe37e4afd3955e
wangyiou <wangyiou12@163.com>
2025-05-27 06:18:33 +0000

    fix

--- test.sh
+++ test.sh
@@ -3,5 +3,7 @@ source log_config.sh
 export PYTHONPATH=${PWD}
 export CIM_COMPILER_BASE=${PWD}
 
-pytest -n 4 test/compiler
-pytest -n 4 test/op/llm/test_attn_decode.py
+python3 test/op/test_conv.py
+# pytest -n 4 test/compiler
+# python3 test/op/llm/test_attn_decode.py
+# cmcp compile -i test/compiler/memory/image/trans/code_.cim -o test_result -c test/compiler/config.json
