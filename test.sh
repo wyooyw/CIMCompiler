f54dcde98c7f7938c124024d2da5239cd49a869f
wangyiou <wangyiou12@163.com>
2025-06-14 11:52:36 +0000

    fix: test.sh

--- test.sh
+++ test.sh
@@ -4,4 +4,6 @@ export PYTHONPATH=${PWD}
 export CIM_COMPILER_BASE=${PWD}
 
 pytest -n 4 test/compiler
-pytest -n 4 test/op/llm/test_attn_decode.py
+pytest -n 4 test/polycim/end2end/test_cimflow_op.py
+pytest test/polycim/end2end/test_cimflow_network.py
+pytest test/op/llm/test_attn_decode.py
